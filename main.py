688e97d19e4122e9ba07e30e9a80d09f02274cd9
Logan Vergara <lvergara@brooklynsteamcenter.org>
2025-02-12 10:02:34 -0500

    Display functionality and database direct connect

--- main.py
+++ main.py
@@ -2,3 +2,54 @@ from flask import Flask, render_template, request, redirect, flash, abort
 import flask_login
 import pymysql
 from dynaconf import Dynaconf
+
+app = Flask(__name__)
+
+conf = Dynaconf(
+    settings_file = ["settings.toml"]
+)
+
+app.secret_key = conf.secret_key
+
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+class User:
+    is_authenticated = True
+    is_anonymous = False
+    is_active = True
+    def __init__(self, user_id, username, email, first_name, last_name):
+        self.id = user_id
+        self.username = username
+        self.email = email
+        self.first_name = first_name
+        self.last_name = last_name
+    def get_id(self):
+        return str(self.id)
+
+@login_manager.user_loader
+
+def load_user(user_id):
+    conn = connectdb()
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT * FROM `Customer` WHERE `id` = {user_id};")
+    result = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    if result is not None:
+        return User(result["id"], result["username"], result["email"], result["first_name"], result["last_name"])
+    
+def connectdb():
+    conn = pymysql.connect(
+        host = "https://db.steamcenter.tech/index.php?route=/&db=plot_point&table=User",
+        database = "",
+        user = conf.username,
+        password = conf.password,
+        autocommit = True,
+        cursorclass = pymysql.cursors.DictCursor
+    )
+    return conn
+
+@app.route("/")
+def index ():
+    return render_template("homepage.html.jinja")
