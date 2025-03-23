9ba8d205fff3a33fd3046fe99e13c5d117d096e7
Cycil <91509720+TIMPRO12@users.noreply.github.com>
2025-03-23 11:53:47 -0400

    10043 44 adding additional stuff the front end (#23)
    * so i can pull jay code
    
    * home

--- main.py
+++ main.py
@@ -240,7 +240,10 @@ def accsin ():
             cursor.close()
             conn.close()
             return redirect("/acc")
+
+
     return render_template ("accsignin.html.jinja")
+
 
 
 @app.route("/", methods=["POST"])
